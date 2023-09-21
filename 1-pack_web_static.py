b99229df73641246a779adc3a43ada8555fabefb
== <==>
2023-09-21 23:49:13 +0000

    Create web_static archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,19 +10,19 @@ from datetime import datetime
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     # obtain the current date and time
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
     # Construct path where archive will be saved
-    archive_path = "versions/web_static_{}.tgz".format(now)
+    archive_path = "versions/web_static_{}.tgz".format(time_stamp)
 
     # use fabric function to create directory if it doesn't exist
     local("mkdir -p versions")
 
     # Use tar command to create a compresses archive
-    archived = local("tar -cvzf {} web_static".format(archive_path))
+    result = local("tar -cvzf {} web_static".format(archive_path))
 
     # Check archive Creation Status
-    if archived.return_code != 0:
+    if result.return_code != 0:
         return None
     else:
         return archive_path
