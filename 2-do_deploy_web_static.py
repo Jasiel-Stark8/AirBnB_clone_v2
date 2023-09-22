2a1c1972f9972e72b3f29178ac7119b1e968b41d
== <==>
2023-09-22 00:32:54 +0000

    distrubute archive to web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,9 +4,8 @@ This Fabric script deploys the web_Static archive to \
 251279-web-01 ubuntu@18.204.5.218 & 251279-web-02 ubuntu@100.26.218.215
 """
 
-import os
+from fabric.api import *
 from datetime import datetime
-from fabric.api import local, put, run, env
 
 # Declare web servers
 env.hosts = ['18.204.5.218', '100.26.218.215']
@@ -33,22 +32,32 @@ def do_pack():
         return archive_path
 
 def do_deploy(archive_path):
-    '''use os module to check for valid file path'''
-    try:
-        if os.path.exists(archive_path):
-            archive = archive_path.split('/')[1]
-            a_path = "/tmp/{}".format(archive)
-            folder = archive.split('.')[0]
-            f_path = "/data/web_static/releases/{}/".format(folder)
+    """Distributes an archive to web servers."""
+    # Check if the archive exists
+    if not exists(archive_path):
+        return False
 
-            put(archive_path, a_path)
-            run("mkdir -p {}".format(f_path))
-            run("tar -xzf {} -C {}".format(a_path, f_path))
-            run("rm {}".format(a_path))
-            run("mv -f {}web_static/* {}".format(f_path, f_path))
-            run("rm -rf {}web_static".format(f_path))
-            run("rm -rf /data/web_static/current")
-            run("ln -s {} /data/web_static/current".format(f_path))
-            return True
+    try:
+        # Extract the archive filename without extension
+        archive_name = archive_path.split("/")[-1]
+        without_extension = archive_name.split(".")[0]
+
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+
+        # Uncompress the archive to the folder on the web server
+        run("mkdir -p /data/web_static/releases/{}/".format(without_extension))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_name, without_extension))
+
+        # Delete the archive from the web server
+        run("rm /tmp/{}".format(archive_name))
+
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link on the web server
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(without_extension))
+
+        return True
     except:
         return False
