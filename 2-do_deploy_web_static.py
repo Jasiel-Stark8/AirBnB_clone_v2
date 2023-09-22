046e94b942b488cea1e82a347942001747000aea
== <==>
2023-09-22 00:45:05 +0000

    distrubute archive to web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -34,21 +34,20 @@ def do_pack():
 
 def do_deploy(archive_path):
     '''use os module to check for valid file path'''
-    try:
-        if os.path.exists(archive_path):
-            archive = archive_path.split('/')[1]
-            a_path = "/tmp/{}".format(archive)
-            folder = archive.split('.')[0]
-            f_path = "/data/web_static/releases/{}/".format(folder)
 
-            put(archive_path, a_path)
-            run("mkdir -p {}".format(f_path))
-            run("tar -xzf {} -C {}".format(a_path, f_path))
-            run("rm {}".format(a_path))
-            run("mv -f {}web_static/* {}".format(f_path, f_path))
-            run("rm -rf {}web_static".format(f_path))
-            run("rm -rf /data/web_static/current")
-            run("ln -s {} /data/web_static/current".format(f_path))
-            return True
-    except:
-        return False
+    if os.path.exists(archive_path):
+        archive = archive_path.split('/')[1]
+        a_path = "/tmp/{}".format(archive)
+        folder = archive.split('.')[0]
+        f_path = "/data/web_static/releases/{}/".format(folder)
+
+        put(archive_path, a_path)
+        run("mkdir -p {}".format(f_path))
+        run("tar -xzf {} -C {}".format(a_path, f_path))
+        run("rm {}".format(a_path))
+        run("mv -f {}web_static/* {}".format(f_path, f_path))
+        run("rm -rf {}web_static".format(f_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(f_path))
+        return True
+    return False
