f8b9a043e3a27d391c4ae99eb93f3cff31d88d12
== <==>
2023-09-23 00:24:54 +0000

    deploy to server web-01

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from os.path import isfile
 
 prv_ky_pth = "my_ssh_private_key"
 env.user = ['ubuntu']
-env.hosts = ["ubuntu@100.25.190.203", "ubuntu@54.175.134.168"]
+env.hosts = ["ubuntu@100.25.190.203"]
 env.key_filename = prv_ky_pth
 
 
