49e472fa6b2bcd13b51b2e17405a5bd067bb955e
== <==>
2023-09-23 22:41:01 +0000

    serve

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,16 @@
-#!/usr/bin/env python3
-"""This script serves a Flask Web Application"""
-# Import Flask
+#!/usr/bin/python3
+"""0. Hello Flask!"""
 from flask import Flask
 
-# Initialize Flask app
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-# Define routes and return static content
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    """Page is served with or without '/' - returns static content"""
-    return "Hello HBNB!"
+    """Display “Hello HBNB!”"""
+    return 'Hello HBNB!'
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
