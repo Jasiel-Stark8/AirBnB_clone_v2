aba599578eca6a76e91142ee87d2f83ad9eb2783
== <==>
2023-09-23 22:27:03 +0000

    Initialize Flask Web Application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This script starts a Flask Web Application"""
+# Imprt Flask
+from flask import Flask
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Define routes and return static content
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
