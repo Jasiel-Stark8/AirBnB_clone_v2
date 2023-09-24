2129ffc17644f0bb24506661584624e67da418a1
== <==>
2023-09-24 23:24:15 +0000

    Jinja update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,8 +12,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def display_states():
     """Render HTML template with all available states from database"""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
