10cf6e3106dd4ae71de49d3022fe716f7ca81208
== <==>
2023-09-24 23:05:43 +0000

    sprt within template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
 """This scripts starts a Flask Web Application"""
 
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from flask import Flask, render_template
 from models import storage
 from models.state import State
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def display_states():
+    """Render HTML template with all available states from database"""
     states = storage.all(State).values()
     states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """Terminate current Database Session"""
+    storage.close()
 
 
 if __name__ == '__main__':
