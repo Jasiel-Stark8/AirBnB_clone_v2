957caeb8da4f95f02c3c69fe7973a901c3e42328
== <==>
2023-09-24 22:58:16 +0000

    Function spacing

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,15 +12,18 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
 
 @app.route('/states_list', strict_slashes=False)
 def display_states():
     states = storage.all(State).values()
     states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
