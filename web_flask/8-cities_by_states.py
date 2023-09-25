bce63988e344d719439d4e5bc771ce517031a749
== <==>
2023-09-25 15:26:14 +0000

    Update template and py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-"""This script starts a Flask Web Application"""
+"""Start a Flask web application"""
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def display_cities_by_states():
-    """Render HTML template with all available states and their cities from database"""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown_db(self):
-    """Terminate current Database Session"""
+def teardown(self):
+    """removes the current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == '__main__':
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_state():
+    """Displays a HTML page with a list of all states & cities"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
