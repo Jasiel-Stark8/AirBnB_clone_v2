75991af2723dfba333cefed9530c76b430e6b11a
== <==>
2023-09-25 16:41:32 +0000

    update

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
-"""This script starts a Flask Web Application"""
+"""Start a Flask web application"""
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Close the storage"""
+def teardown(self):
+    """removes the current SQLAlchemy Session"""
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
 def states():
-    """Display a HTML page with the list of all State objects"""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('9-states.html', states=states)
+    """Displays a HTML page with a list of all states & cities"""
+    states = storage.all(State)
+    return render_template('9-states.html', state=states)
 
 
-@app.route('/states/<id>', strict_slashes=False)
-def state_id(id):
-    """Display a HTML page with the list of City objects linked to the State"""
-    states = storage.all(State).values()
-    state = next((state for state in states if state.id == id), None)
-    return render_template('9-states.html', state=state)
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
