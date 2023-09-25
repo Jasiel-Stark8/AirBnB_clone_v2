5063e5a199d9ba4ca0592614b078b874bf99f601
== <==>
2023-09-25 14:24:18 +0000

    Display cities_by_states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
+"""This script Displays citeis by States"""
 
-from flask import Flask, render_template
 from models import storage
 from models.state import State
-
+from models.city import City
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """removes the current SQLAlchemy Session"""
+def teardown_db(self):
+    """Terminate current Database Session"""
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_state():
-    """Displays a HTML page with a list of all states & cities"""
-    states = storage.all(State)
+@app.route('/cities_by_states')
+def display_cities():
+    """This displays cities"""
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
+    states = sorted(states, key=lambda state: state.name)
+    cities = sorted(cities, key=lambda city: city.name)
     return render_template('8-cities_by_states.html', states=states)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
