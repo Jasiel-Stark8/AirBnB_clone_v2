7a259c3d7dbc3b78b4146e5f58a78bc613e6a4fb
== <==>
2023-09-25 15:05:06 +0000

    Displays cities by States

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,7 +5,9 @@ from models import storage
 from models.state import State
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/cities_by_states', strict_slashes=False)
 def display_cities_by_states():
@@ -14,10 +16,12 @@ def display_cities_by_states():
     states = sorted(states, key=lambda state: state.name)
     return render_template('8-cities_by_states.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Terminate current Database Session"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
